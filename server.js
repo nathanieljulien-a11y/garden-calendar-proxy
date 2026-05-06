3355f11f396672005ada098aec5f22a60d7d7bdb
nathanieljulien-a11y <nathanieljulien@gmail.com>
2026-05-06 21:32:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-gc-token, x-admin-secret');
   res.setHeader('Access-Control-Max-Age', '86400');
   if (req.method === 'OPTIONS') { res.sendStatus(204); return; }
   next();
