886efce8f21cdb71317d9ea42b492944bc097b7b
nathanieljulien-a11y <nathanieljulien@gmail.com>
2026-06-01 19:13:55 +0100

    Update garden-wall-calendar.js

--- products/garden-wall-calendar.js
+++ products/garden-wall-calendar.js
@@ -83,7 +83,15 @@ function getCommentary(plant) {
 // Display name overrides for plant keys that don't match their common name
 var PLANT_DISPLAY = {
   lilyofthevalley: 'Lily of the Valley',
-  limetree:        'Lime Tree',
+  limetree: 'Lime Tree',
+  autumncrocus: 'Autumn Crocus',
+  baylaurel: 'Bay Laurel',
+  wildpansy: 'Wild Pansy',
+  wildthyme: 'Wild Thyme',
+  sourcherry: 'Sour Cherry',
+  gardenmint: 'Garden Mint',  // worth adding while you're there
+  lemonbalm: 'Lemon Balm',   // and this one
+  wildpansy: 'Wild Pansy',
 };
 
 var ARTWORK_SOURCES = {
